0783e07419ec8491bd8b846ffc03a57d027f714c
gholamzadehhojjat1997 <gholamzadeh.hojjat.1997@gmail.com>
2024-05-18 10:58:26 +0330

    updateAFeature

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ addButton.addEventListener("click", () => {
     let taskText = document.createTextNode(task.value);
     ul.appendChild(li);
     li.innerHTML += `<span class="trash"><i class="fa-solid fa-trash-can"></i></span>
-                      <span class="done"><i class="fa-regular fa-square-check"></i></span>
+                      <span class="done"><i class="fa-regular fa-square"></i></span>
                       <span class="edition"><i class="fa-solid fa-pen"></i></span>`;
     li.appendChild(taskText);
     task.value = "";
@@ -18,8 +18,23 @@ addButton.addEventListener("click", () => {
 ul.addEventListener("click", (event) => {
   if (event.target.className === "fa-solid fa-trash-can") {
     event.target.parentElement.parentElement.style.display = "none";
-  } else if (event.target.className === "fa-regular fa-square-check") {
+  } else if (event.target.className === "fa-regular fa-square") {
+    event.target.parentElement.firstElementChild.classList.toggle("fa-square");
+    event.target.parentElement.firstElementChild.classList.toggle(
+      "fa-square-check"
+    );
     event.target.parentElement.parentElement.classList.toggle("taskSituation");
-    event.target.parentElement.previousElementSibling.classList.toggle("trashSituation");
+    event.target.parentElement.previousElementSibling.classList.toggle(
+      "trashSituation"
+    );
+  } else if (event.target.className === "fa-regular fa-square-check") {
+    event.target.parentElement.firstElementChild.classList.toggle("fa-square");
+    event.target.parentElement.firstElementChild.classList.toggle(
+      "fa-square-check"
+    );
+    event.target.parentElement.parentElement.classList.toggle("taskSituation");
+    event.target.parentElement.previousElementSibling.classList.toggle(
+      "trashSituation"
+    );
   }
 });
